8ce96d51646465d283adab3203dd803ee6539a7f
yuanjun <yuanjun@papayamobile.com>
2016-03-07 20:17:38 +0800

    ignore

---
+++ tools/kNNtest.py
@@ -0,0 +1,85 @@
+__author__ = 'quantum'
+# -*- coding: utf-8 -*-
+import numpy as np
+from sklearn import neighbors
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import classification_report
+from sklearn.cross_validation import train_test_split
+import matplotlib.pyplot as plt
+from tools import df
+
+'''read data'''
+data   = []
+labels = []
+#with open("../Data/1.txt") as ifile:
+        #for line in ifile:
+            #tokens = line.strip().split(' ')
+            #data.append([float(tk) for tk in tokens[:-1]])
+            #labels.append(tokens[-1])
+
+
+for i in xrange(len(df)):
+    item = [ df[column][i] for column in df.columns if ((not isinstance(df[column][i], basestring)) and (column != "CONSUMER_ID"))]
+    item = np.array(item)
+    #print "$$$$$$$",np.isnan(item)
+    if True in np.isnan(item):
+        print "have null value"
+    else:
+        data.append([ float(df[column][i]) for column in df.columns if ((not isinstance(df[column][i], basestring)) and (column != "CONSUMER_ID"))])
+        labels.append(df['DEMO_X'][i])
+print 'labels ==',labels
+
+x = np.array(data)
+print "#####",x
+labels = np.array(labels)
+#y = np.zeros(labels.shape)
+y = labels
+print "*******",y
+
+''' change to 0/1 '''
+#y[labels=='fat']=1
+print "y==",y
+
+print '-------------------------------'
+print len(x)
+print len(y)
+print '--------------------------------'
+
+
+''' diff test data and train data '''
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
+
+''' draw pic '''
+#h = .01
+#x_min, x_max = x[:, 0].min() - 0.1, x[:, 0].max() + 0.1
+#y_min, y_max = x[:, 1].min() - 1, x[:, 1].max() + 1
+#xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+ #                    np.arange(y_min, y_max, h))
+
+''' train KNN '''
+clf = neighbors.KNeighborsClassifier(algorithm='kd_tree')
+clf.fit(x_train, y_train)
+
+''' test and print'''
+answer = clf.predict(x)
+print "test and print"
+#print(x)
+print(answer)
+print(y)
+print(np.mean( answer == y))
+
+''' precise and recall  '''
+precision, recall, thresholds = precision_recall_curve(y_train, clf.predict(x_train))
+answer = clf.predict_proba(x)[:,1]
+#print(classification_report(y, answer, target_names = ['thin', 'fat']))
+
+
+answer = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:,1]
+z = answer.reshape(xx.shape)
+plt.contourf(xx, yy, z, cmap=plt.cm.Paired, alpha=0.8)
+
+''' paint train data'''
+plt.scatter(x_train[:, 0], x_train[:, 1], c=y_train, cmap=plt.cm.Paired)
+plt.xlabel('high')
+plt.ylabel('weight')
+plt.show()
--- tools/tools.py
+++ tools/tools.py
@@ -2,8 +2,23 @@
 #-*- coding: utf:8 -*-
 import pandas as pd
 from setting import DATA_DIR
+#from sklearn import neighbors
+#from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+import numpy as np
+from sklearn import neighbors
+from sklearn.cross_validation import train_test_split
 
 df = pd.read_csv(DATA_DIR+'/demographic_membership_training.csv')
+
+#knn = neighbors.KNeighborsClassifier(n_neighbors = 2)
+
+#clf = LogisticRegression()
+
+#print knn
+
+#clf.fit([[1],[2],[3],[4],[5],[6]], [0,0,0,1,1,1])
+#print "**********",clf.predict([2])
 
 
 
@@ -14,14 +29,25 @@ class DemographicMembership():
 
 
 if __name__ == "__main__":
-    print '++++++++',__file__
-    print sum(df['DEMO_X'])
-    print '####',len(dir(df))
-    #for item in dir(df):
-    #   print k,v
-        #print item
-    print dir(df)
-    print df.columns
-    print len(df)
+    #print sum(df['DEMO_X'])
+    #print dir(df.columns)
+    trainlist = []
+    resultlist = []
+    trainlist = np.array(trainlist)
+    #clf_linear  = svm.SVC(kernel='linear').fit(trainlist, resultlist)
+    for i in xrange(20):
+        trainlist.append([ df[column][i] for column in df.columns if ((not isinstance(df[column][i], basestring)) and (column != "CONSUMER_ID"))])
+        resultlist.append(df['DEMO_X'][i])
+    #resultlist = np.zeros(resultlist)
+    x_train, x_test, y_train, y_test = train_test_split(trainlist, resultlist, test_size = 0.2)
+
+    print '**************&&&&&&&&&',len(x_train),len(y_train)
+    clf = neighbors.KNeighborsClassifier(algorithm='kd_tree')
+
+    clf.fit(x_train, y_train)
+    print trainlist[1]
+    #print '-------------------------------',resultlist
+    #knn.fit(trainlist,resultlist)
+    #print "##########", clf_linear.predict(trainlist[10]), resultlist[10]
     print "ok"
 
